47dff53aa7dbc6715e7f9dfe86b1bfe063da2c9c
jeffward01 <jeffwardcoding@gmail.com>
2015-10-23 19:02:58 -0700

    added twitter login

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-//Modal Knockout
-ko.validation.rules.pattern.message = 'Invalid';
-
-ko.validation.init({
-    registerExtenders: true,
-    messagesOnModified: true,
-    insertMessages: true,
-    parseInputAttributes: true,
-    messageTemplate: null
-}, true);
+////Modal Knockout
+//ko.validation.rules.pattern.message = 'Invalid';
+//
+//ko.validation.init({
+//    registerExtenders: true,
+//    messagesOnModified: true,
+//    insertMessages: true,
+//    parseInputAttributes: true,
+//    messageTemplate: null
+//}, true);
 
